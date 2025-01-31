643f6f7844b4e0e2aec9ae949cc2d1698db5928c
letmezep <letmezep@gmail.com>
2025-01-31 17:21:22 +0800

    refactor: transfered a data request to a separate module

--- src/components/ResultList/ResultList.tsx
+++ src/components/ResultList/ResultList.tsx
@@ -1,32 +1,34 @@
 import { Component } from 'react';
-import getListFromApi from '../../services/api/getApiList';
 import { Character, Data } from '../../services/interfaces';
+import { fetchData } from '../../services/api/fetchData';
 
 export default class ResultList extends Component {
-  state: { data: Data | null } = {
-    // data: localStorage.getItem('searchTerm') || 'STATE fallback term',
+  state: { data: Data | null; loading: boolean; error: null } = {
     data: null,
+    loading: true,
+    error: null,
   };
 
   async componentDidMount(): Promise<void> {
-    try {
-      const response: Data = await getListFromApi();
-      this.setState({ data: response });
-    } catch (error) {
-      console.error('Error fetch', error);
+    const data = await fetchData();
+    if (data) {
+      this.setState({ data, loading: false });
+    } else {
+      this.setState({ error: 'failed to load FETCHDATA', loading: false });
     }
   }
 
   render() {
-    const { data } = this.state;
+    const { data, loading, error } = this.state;
 
+    if (loading) return <p>Now is Loading...</p>;
+    if (error) return <p>{error}</p>;
     if (!data) {
       return <p>Now is Loading...</p>;
     }
+
     return (
       <>
-        {/* <p>{data.results?.[3].name || 'No results'}</p> */}
-
         {data.results.map((item: Character, index: number) => (
           <div className="list-item" key={index}>
             <h4>{item.name}</h4>
@@ -40,42 +42,3 @@ export default class ResultList extends Component {
     );
   }
 }
-
-// import React, { useState, useEffect } from 'react';
-// import getListFromApi from '../../services/api/getApiList';
-// import { Character, Data } from '../../services/interfaces';
-
-// function ResultList() {
-//   const [data, setData] = useState<Data>({ results: [] });
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const result = await getListFromApi();
-//       setData(result);
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <>
-//       <div className="main-field__container">
-//         {data.results && (
-//           <div className="list-item__container">
-//             {data.results.map((item: Character, index: number) => (
-//               <div className="list-item" key={index}>
-//                 <h4>{item.name}</h4>
-//                 <p>
-//                   Height: {item.height} Mass: {item.mass} Hair Color:{' '}
-//                   {item.hair_color}
-//                 </p>
-//               </div>
-//             ))}
-//           </div>
-//         )}
-//       </div>
-//     </>
-//   );
-// }
-
-// export default ResultList;

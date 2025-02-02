8fb93de6d7ec2bac5f7465201cac482e3a8f4ea5
letmezep <letmezep@gmail.com>
2025-02-02 17:51:55 +0800

    refactor: replace error boundary file in components dir

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
-// import './index.css';
-import ErrorBoundary from './services/ErrorBoundary';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const rootElement = document.getElementById('root');
 

acca14e66813cc76fe273863d429d8209504e199
letmezep <letmezep@gmail.com>
2025-02-12 23:58:46 +0800

    refactor: migrate error button to functional component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react';
 // import ResultList from '../components/layout/CardList';
 import Header from '../components/layout/Header';
-import ErrorButton from '../components/ui/ErrorButton';
+import ErrorButton from '../components/common/ErrorButton';
 // import { localStorageItem } from '../constants';
 // import React, { useEffect, useState } from 'react';
 

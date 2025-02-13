bbdfe148646b812901cefedb77cbf8d34d4e41d8
letmezep <letmezep@gmail.com>
2025-02-13 23:34:53 +0800

    feat: implement page not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import MainPage from './pages/MainPage';
+import { Routes, Route } from 'react-router';
+import NotFound from './pages/NotFound';
 
 const App: React.FC = () => {
   return (
-    <>
-      <MainPage />
-    </>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 };
 

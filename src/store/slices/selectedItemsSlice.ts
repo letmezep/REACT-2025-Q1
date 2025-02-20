4e3cc1bbcc8f1212556a8b7abcf8d0e5a3e893bd
letmezep <letmezep@gmail.com>
2025-02-20 13:53:38 +0800

    refactor: clean and improve code with use api call by rtk query

--- src/store/slices/selectedItemsSlice.ts
+++ src/store/slices/selectedItemsSlice.ts
@@ -1,30 +1,31 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Character } from '../../types/interfaces';
+// import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+// import { RootState } from '../store';
+// import { Character } from '../../types/interfaces';
 
-interface SelectedItemsState {
-  selectedItems: Record<string, Character>;
-}
+// interface SelectedItemsState {
+//   selectedItems: Record<string, Character>;
+// }
 
-const initialState: SelectedItemsState = {
-  selectedItems: {},
-};
+// const initialState: SelectedItemsState = {
+//   selectedItems: {},
+// };
 
-const selectedItemsSlice = createSlice({
-  name: 'selectedItems',
-  initialState,
-  reducers: {
-    toggleItemSelection: (state, action: PayloadAction<Character>) => {
-      const { url } = action.payload;
-      if (state.selectedItems[url]) {
-        delete state.selectedItems[url];
-      } else {
-        state.selectedItems[url] = action.payload;
-      }
-    },
-  },
-});
+// const selectedItemsSlice = createSlice({
+//   name: 'selectedItems',
+//   initialState,
+//   reducers: {
+//     toggleItemSelection: (state, action: PayloadAction<Character>) => {
+//       const { url } = action.payload;
+//       if (state.selectedItems[url]) {
+//         delete state.selectedItems[url];
+//       } else {
+//         state.selectedItems[url] = action.payload;
+//       }
+//     },
+//   },
+// });
 
-export const { toggleItemSelection } = selectedItemsSlice.actions;
+// export const { toggleItemSelection } = selectedItemsSlice.actions;
 // export const selectSelectedItems = (state: RootState) =>
 //   state.selectedItems.selectedItems;
-export default selectedItemsSlice.reducer;
+// export default selectedItemsSlice.reducer;

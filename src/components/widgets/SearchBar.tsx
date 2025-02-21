1594dd4df32479f3625f0998b332e8aae4423232
letmezep <letmezep@gmail.com>
2025-02-21 11:22:58 +0800

    fix: pagination. error in an array of dependencies in search bar component

--- src/components/widgets/SearchBar.tsx
+++ src/components/widgets/SearchBar.tsx
@@ -16,7 +16,7 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearchChange }) => {
   useEffect(() => {
     onSearchChange(searchTerm);
     setSearchParams({ search: searchTerm, page: '1' });
-  }, [searchTerm, onSearchChange, setSearchParams]);
+  }, [searchTerm]);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
@@ -49,49 +49,3 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearchChange }) => {
 };
 
 export default SearchBar;
-
-// import React, { useEffect, useState } from 'react';
-// import BaseButton from '../ui/BaseButton';
-// import BaseInput from '../ui/BaseInput';
-// import { useSearchQuery } from '../../hooks/useSearchQuery';
-// import ErrorButton from '../common/ErrorButton';
-// import { useSearchParams } from 'react-router';
-
-// import { SearchBarProps } from '../../types/interfaces';
-
-// const SearchBar: React.FC<SearchBarProps> = ({ onSearchChange }) => {
-//   const { searchTerm, setSearchTerm } = useSearchQuery();
-//   const [, setSearchParams] = useSearchParams();
-//   const [inputValue, setInputValue] = useState(
-//     localStorage.getItem('searchTerm') || ''
-//   );
-
-//   useEffect(() => {
-//     onSearchChange(searchTerm);
-//     setSearchParams({ search: searchTerm, page: '1' });
-//   }, [searchTerm, onSearchChange, setSearchParams]);
-
-//   const handleSearchClick = () => {
-//     setSearchTerm(inputValue);
-//     localStorage.setItem('searchTerm', searchTerm);
-//   };
-
-//   return (
-//     <div className="search-field__container">
-//       <BaseInput
-//         className="search-field__input"
-//         type="text"
-//         placeholder="Input search term"
-//         value={inputValue}
-//         onChange={(e) => setInputValue(e.target.value)}
-//       />
-//       <BaseButton className="search-field__button" onClick={handleSearchClick}>
-//         Search
-//       </BaseButton>
-
-//       <ErrorButton />
-//     </div>
-//   );
-// };
-
-// export default SearchBar;

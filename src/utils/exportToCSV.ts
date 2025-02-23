7add57b0cfeaac8158d27ff2b9ff56a4c065dd5a
letmezep <letmezep@gmail.com>
2025-02-23 22:22:13 +0800

    refactor: download without dom manipulation

--- src/utils/exportToCSV.ts
+++ src/utils/exportToCSV.ts
@@ -1,4 +1,4 @@
-import { Character } from '../types/interfaces';
+import { Character } from "../types/interfaces";
 
 export const exportToCSV = (selectedItems: Character[]) => {
   if (selectedItems.length === 0) return;
@@ -22,13 +22,46 @@ export const exportToCSV = (selectedItems: Character[]) => {
 
   const csvContent = csvRows.join('\n');
 
-  const fileName = `${selectedItems.length}_items.csv`;
-
   const blob = new Blob([csvContent], { type: 'text/csv' });
-  const link = document.createElement('a');
-  link.href = URL.createObjectURL(blob);
-  link.download = fileName;
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
+  const url = URL.createObjectURL(blob);
+
+  window.open(url);
+
+  setTimeout(() => URL.revokeObjectURL(url), 1000);
 };
+
+
+// import { Character } from '../types/interfaces';
+
+// export const exportToCSV = (selectedItems: Character[]) => {
+//   if (selectedItems.length === 0) return;
+
+//   const headers = ['Name', 'Description', 'Details URL'];
+
+//   const csvRows = [
+//     headers.join(','),
+//     ...selectedItems.map((item) =>
+//       [
+//         `name: ${item.name}`,
+//         `birth year: ${item.birth_year}`,
+//         `gender: ${item.gender}`,
+//         `height: ${item.height}`,
+//         `mass: ${item.mass}`,
+//         `skin color: ${item.skin_color}`,
+//         `url: ${item.url}`,
+//       ].join(', ')
+//     ),
+//   ];
+
+//   const csvContent = csvRows.join('\n');
+
+//   const fileName = `${selectedItems.length}_items.csv`;
+
+//   const blob = new Blob([csvContent], { type: 'text/csv' });
+//   const link = document.createElement('a');
+//   link.href = URL.createObjectURL(blob);
+//   link.download = fileName;
+//   document.body.appendChild(link);
+//   link.click();
+//   document.body.removeChild(link);
+// };

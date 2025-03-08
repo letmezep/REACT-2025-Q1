c2ae4b736ad7e31114561ad590e3d29cde235341
letmezep <letmezep@gmail.com>
2025-03-08 22:11:24 +0800

    refactor: remove extra strings in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -24,12 +24,6 @@ export default [
     files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       parser: tsParser,
-      // parser: tseslint.parser,
-      // parserOptions: {
-      //   project: './tsconfig.json',
-      //   // project: './tsconfig.eslint.json',
-      //   sourceType: 'module',
-      // },
       ecmaVersion: 2020,
       globals: globals.browser,
     },
@@ -65,100 +59,4 @@ export default [
       },
     },
   },
-  // {
-  //   // files: ['**/*.{ts,tsx,js,jsx}'],
-  //   files: ['components/**/*.{ts,tsx,js}', 'pages/**/*.{ts,tsx,js,jsx}'],
-  //   languageOptions: {
-  //     ecmaVersion: 'latest',
-  //     sourceType: 'module',
-  //     globals: globals.browser,
-  //     parser: tseslint.parser,
-  //     parserOptions: {
-  //       project: './tsconfig.json',
-  //       sourceType: 'module',
-  //     },
-  //   },
-  //   plugins: {
-  //     '@next/next': next,
-  //     react,
-  //     'react-hooks': reactHooks,
-  //     'react-refresh': reactRefresh,
-  //     'react-compiler': reactCompiler,
-  //     prettier: eslintPluginPrettier,
-  //   },
-  //   rules: {
-  //     ...js.configs.recommended.rules,
-  //     ...tseslint.configs.strict.rules,
-  //     ...react.configs.recommended.rules,
-  //     ...react.configs['jsx-runtime'].rules,
-  //     ...reactHooks.configs.recommended.rules,
-  //     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-
-  //     'react/react-in-jsx-scope': 'off',
-  //     'react-refresh/only-export-components': [
-  //       'warn',
-  //       { allowConstantExport: true },
-  //     ],
-  //     'react-compiler/react-compiler': 'error',
-  //     'react/prop-types': 'off',
-  //     'prettier/prettier': 'error',
-  //   },
-  //   settings: {
-  //     react: {
-  //       version: 'detect',
-  //     },
-  //   },
-  // },
 ];
-
-// import next from '@next/eslint-plugin-next';
-// import js from '@eslint/js';
-// import globals from 'globals';
-// import reactHooks from 'eslint-plugin-react-hooks';
-// import reactRefresh from 'eslint-plugin-react-refresh';
-// import react from 'eslint-plugin-react';
-// import tseslint from 'typescript-eslint';
-// import eslintPluginPrettier from 'eslint-plugin-prettier';
-// import reactCompiler from 'eslint-plugin-react-compiler';
-
-// export default [
-//   {
-//     ignores: ['dist', 'coverage'],
-//   },
-//   {
-//     files: ['**/*.{ts,tsx,js,jsx}'],
-//     languageOptions: {
-//       ecmaVersion: 2020,
-//       globals: globals.browser,
-//     },
-//     plugins: {
-//       '@next/next': next,
-//       react,
-//       'react-hooks': reactHooks,
-//       'react-refresh': reactRefresh,
-//       'react-compiler': reactCompiler,
-//     },
-//     rules: {
-//       ...js.configs.recommended.rules,
-//       ...tseslint.configs.strict.rules,
-//       ...react.configs.recommended.rules,
-//       ...react.configs['jsx-runtime'].rules,
-//       ...reactHooks.configs.recommended.rules,
-//       'react/react-in-jsx-scope': 'off',
-//       'react-refresh/only-export-components': [
-//         'warn',
-//         { allowConstantExport: true },
-//       ],
-//       'react-compiler/react-compiler': 'error',
-//       'react/prop-types': 'off',
-//     },
-//     settings: {
-//       react: {
-//         version: 'detect',
-//       },
-//     },
-//   },
-//   eslintPluginPrettier.configs.recommended,
-//   next.configs.recommended,
-//   next.configs['core-web-vitals'],
-// ];

ce28d088818916eca79ae5733449d85d99887719
letmezep <letmezep@gmail.com>
2025-03-12 15:09:43 +0800

    feat: add redux toolkit to project

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 import './index.css';
 import App from './App.tsx';
 
@@ -7,7 +9,9 @@ const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </StrictMode>
   );
 } else {

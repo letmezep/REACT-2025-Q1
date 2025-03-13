24e6a59d6568e45487701661907c91b6b97782e1
letmezep <letmezep@gmail.com>
2025-03-13 21:47:18 +0800

    feat: add react hook form to the project

--- src/pages/FormHook.tsx
+++ src/pages/FormHook.tsx
@@ -1,5 +1,29 @@
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { submitForm } from '../store/slices/formSlice';
+import { FormState } from '../types/interfaces';
+
 const FormHook = () => {
-  return <div>form created using React Hook Form</div>;
+  const navigate = useNavigate();
+
+  const { register, handleSubmit } = useForm<FormState>();
+  const dispatch = useDispatch();
+
+  const onSubmit = (data: FormState) => {
+    dispatch(submitForm({ ...data }));
+    navigate('/');
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>React Hook Form</h1>
+        <input {...register('name')} placeholder="name" />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 };
 
 export default FormHook;
